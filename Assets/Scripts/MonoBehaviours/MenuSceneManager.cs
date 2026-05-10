36f943960ad65866beb0ee5fd20523bb41ccad92
Shintaro Mukai <shintaro@mukai.systems>
2026-05-10 14:35:50 +0900

    Wire up DemoNoticePanel close button in demo builds.

--- Assets/Scripts/MonoBehaviours/MenuSceneManager.cs
+++ Assets/Scripts/MonoBehaviours/MenuSceneManager.cs
@@ -216,6 +216,7 @@ public class MenuSceneManager : MonoBehaviour
         QuitButton.onClick.AddListener(OnPowerOff);
 #if DEMO
         DemoNoticePanel.SetActive(true);
+        DemoNoticePanel.GetComponentInChildren<Button>().onClick.AddListener(() => DemoNoticePanel.SetActive(false));
 #endif
         ChangeState(State.None);
     }
